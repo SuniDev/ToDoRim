2f368ebfcbb844687997acfa3ccea5e514906fef
SuniDev <sun-918@naver.com>
2024-05-17 14:47:57 +0900

    [feat] set up project resources
    #2

--- TodorimMVC/TodorimMVC/Resources/Colors.swift
+++ TodorimMVC/TodorimMVC/Resources/Colors.swift
@@ -13,6 +13,8 @@
 #endif
 
 // Deprecated typealiases
+@available(*, deprecated, renamed: "ColorAsset.Color", message: "This typealias will be removed in SwiftGen 7.0")
+internal typealias AssetColorTypeAlias = ColorAsset.Color
 
 // swiftlint:disable superfluous_disable_command file_length implicit_return
 
@@ -20,10 +22,96 @@
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
 internal enum Colors {
+  internal static let accent = ColorAsset(name: "accent")
+  internal static let mapFill = ColorAsset(name: "map_fill")
+  internal static let mapStroke = ColorAsset(name: "map_stroke")
+  internal static let red = ColorAsset(name: "red")
+  internal static let bottom1 = ColorAsset(name: "bottom1")
+  internal static let bottom2 = ColorAsset(name: "bottom2")
+  internal static let bottom3 = ColorAsset(name: "bottom3")
+  internal static let bottom4 = ColorAsset(name: "bottom4")
+  internal static let bottom5 = ColorAsset(name: "bottom5")
+  internal static let bottom6 = ColorAsset(name: "bottom6")
+  internal static let bottom7 = ColorAsset(name: "bottom7")
+  internal static let bottom8 = ColorAsset(name: "bottom8")
+  internal static let bottom9 = ColorAsset(name: "bottom9")
+  internal static let top1 = ColorAsset(name: "top1")
+  internal static let top2 = ColorAsset(name: "top2")
+  internal static let top3 = ColorAsset(name: "top3")
+  internal static let top4 = ColorAsset(name: "top4")
+  internal static let top5 = ColorAsset(name: "top5")
+  internal static let top6 = ColorAsset(name: "top6")
+  internal static let top7 = ColorAsset(name: "top7")
+  internal static let top8 = ColorAsset(name: "top8")
+  internal static let top9 = ColorAsset(name: "top9")
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
+
+internal final class ColorAsset {
+  internal fileprivate(set) var name: String
+
+  #if os(macOS)
+  internal typealias Color = NSColor
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  internal typealias Color = UIColor
+  #endif
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  internal private(set) lazy var color: Color = {
+    guard let color = Color(asset: self) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }()
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 11.0, tvOS 11.0, *)
+  internal func color(compatibleWith traitCollection: UITraitCollection) -> Color {
+    let bundle = BundleToken.bundle
+    guard let color = Color(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }
+  #endif
+
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+  internal private(set) lazy var swiftUIColor: SwiftUI.Color = {
+    SwiftUI.Color(asset: self)
+  }()
+  #endif
+
+  fileprivate init(name: String) {
+    self.name = name
+  }
+}
+
+internal extension ColorAsset.Color {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  convenience init?(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+#if canImport(SwiftUI)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+internal extension SwiftUI.Color {
+  init(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle)
+  }
+}
+#endif
 
 // swiftlint:disable convenience_type
 private final class BundleToken {
