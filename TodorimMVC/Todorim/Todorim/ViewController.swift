ff44dbad509d98cd7d6d8bdd8b60f9a165008a8b
SuniDev <sun-918@naver.com>
2024-08-12 00:11:15 +0900

    init Project

---
+++ TodorimMVC/Todorim/Todorim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todorim
+//
+//  Created by suni on 8/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodorimMVC/Todorim/TodorimUITests/TodorimUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TodorimUITestsLaunchTests.swift
+//  TodorimUITests
+//
+//  Created by suni on 8/12/24.
+//
+
+import XCTest
+
+final class TodorimUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
