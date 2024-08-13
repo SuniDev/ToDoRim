a2e585fed281c04dadb1aa82512df13f09bb57bd
SuniDev <sun-918@naver.com>
2024-08-13 18:32:25 +0900

    refactor : 미사용 코드 삭제
    #10

--- Todorim/Todorim/Common/Extensions/UIColor+Extension.swift
+++ Todorim/Todorim/Common/Extensions/UIColor+Extension.swift
@@ -38,4 +38,20 @@ extension UIColor {
         let rgba = (Int(a * 255) << 24) | (Int(r * 255) << 16) | (Int(g * 255) << 8) | Int(b * 255)
         return String(format: "#%08x", rgba)
     }
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
 }
--- Todorim/Todorim/Sources/V1/Utils/Extensions/DoubleExtension.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DoubleExtension.swift
-//  HSTODO
-//
-//  Created by 박현선 on 26/09/2019.
-//  Copyright © 2019 hspark. All rights reserved.
-//
-
-import Foundation
