21824a4c61846e74c492b2f6caba042ef6118e80
SuniDev <sun-918@naver.com>
2024-08-15 23:40:49 +0900

    refactor : 위치 검색 VC 추가
    #10

---
+++ Todorim/Todorim/Controller/SearchLocation/SearchLocationMapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchLocationMapViewController.swift
+//  Todorim
+//
+//  Created by suni on 8/15/24.
+//
+
+import UIKit
+
+class SearchLocationMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Todorim/Todorim/Controller/SearchLocation/SearchLocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchLocationViewController.swift
+//  Todorim
+//
+//  Created by suni on 8/15/24.
+//
+
+import UIKit
+
+class SearchLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
