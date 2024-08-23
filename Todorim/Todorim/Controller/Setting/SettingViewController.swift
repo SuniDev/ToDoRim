efb12a48b417de788f1ce868f30ddf9f816516b9
SuniDev <sun-918@naver.com>
2024-08-23 11:44:44 +0900

    feat : 설정 화면 UI 개발
    #21

---
+++ Todorim/Todorim/Controller/Setting/SettingViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SettingViewController.swift
+//  Todorim
+//
+//  Created by suni on 8/23/24.
+//
+
+import UIKit
+
+class SettingViewController: BaseViewController {
+    
+    // MARK: - DATA
+    
+    // MARK: - Outlet
+    @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var updateLabel: UILabel!
+    @IBOutlet weak var gadLabel: UILabel!
+    
+    // MARK: - Action
+    @IBAction func tappedBackButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedDeleteGAD(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedGoReview(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedContactUs(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedUpdate(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedPrivacyPolicy(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tappedTermsOfService(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
